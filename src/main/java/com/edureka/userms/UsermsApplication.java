6555817dd0811e9838dd5b23f6aa07b0405b4760
Amritendu De <amritendu_de@yahoo.com>
2019-11-03 19:59:47 +0530

    WAR file conversion

--- src/main/java/com/edureka/userms/UsermsApplication.java
+++ src/main/java/com/edureka/userms/UsermsApplication.java
@@ -2,12 +2,18 @@ package com.edureka.userms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UsermsApplication {
+public class UsermsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsermsApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(UsermsApplication.class);
+	}
 }
